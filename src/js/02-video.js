44d5c79960e2463b2af79c6b1962fce2d64a6310
Renat <72465313+teenrage-dev@users.noreply.github.com>
2022-05-25 23:57:24 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+debugger
+const onPlay = function(data) {
+    data: {
+        duration: 61.857,
+        percent: 0.049,
+        seconds: 3.034,
+        customKey: 'videoplayer-current-time'
+    },
+};
+
+
+console.log(player.on('timeupdate', onPlay));
